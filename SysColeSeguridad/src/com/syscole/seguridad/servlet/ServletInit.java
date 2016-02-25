da9e9628165dee5aa6dfe1a58bf304abf844fc8d
ernestodavides <ernesto.davides@gmail.com>
2016-02-24 23:11:54 -0600

    Avances

--- SysColeSeguridad/src/com/syscole/seguridad/servlet/ServletInit.java
+++ SysColeSeguridad/src/com/syscole/seguridad/servlet/ServletInit.java
@@ -1,6 +1,8 @@
 package com.syscole.seguridad.servlet;
 
 import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -10,7 +12,7 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class ServletInit
  */
-@WebServlet("/ServletInit")
+@WebServlet("/portal/login")
 public class ServletInit extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
@@ -26,7 +28,9 @@ public class ServletInit extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+		RequestDispatcher dispatcher = request.getRequestDispatcher("/pages/index.html");
+		dispatcher.forward(request, response);
+//		response.sendRedirect("pages/index.html");
 	}
 
 	/**
